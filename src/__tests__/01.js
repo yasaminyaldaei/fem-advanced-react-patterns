ca565073532506495c32f2800394b1076f6cbd1b
Yasamin Yaldaei <y.yaldaei@digikala.com>
2020-11-10 19:05:33 +0330

    email autofill

--- src/__tests__/01.js
+++ src/__tests__/01.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {renderToggle} from '../../test/utils'
-import Usage from '../exercises-final/01'
-// import Usage from '../exercises/01'
+// import Usage from '../exercises-final/01'
+import Usage from '../exercises/01'
 
 test('renders a toggle component', () => {
   const handleToggle = jest.fn()
@@ -22,7 +22,7 @@ test('renders a toggle component', () => {
 // 3. Change submitted from `false` to `true`
 // 4. And you're all done!
 /*
-http://ws.kcd.im/?ws=react%20patterns&e=01&em=
+http://ws.kcd.im/?ws=react%20patterns&e=01&em=yasaminyaldaei@gmail.com
 */
 test.skip('I submitted my elaboration and feedback', () => {
   const submitted = false // change this when you've submitted!
--- src/exercises/01.js
+++ src/exercises/01.js
@@ -3,17 +3,24 @@
 import React from 'react'
 // 🐨 uncomment this import to get the switch component.
 // It takes an `onClick` and an `on` prop
-// import {Switch} from '../switch'
+import { Switch } from '../switch'
 
 class Toggle extends React.Component {
   // 🐨 this toggle component is going to need to have state for `on`
-  //
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      on: false,
+    }
+  }
+  onToggle = () => this.setState(({ on }) => ({ on: !on }), () => this.props.onToggle(this.state.on))
   // You'll also want a method to handle when the switch is clicked
   // which will update the `on` state and call the `onToggle` prop
   // 💰 this.props.onToggle(this.state.on)
   render() {
     // 🐨 here you'll want to return the switch with the `on` and `onClick` props
-    return null
+    return <Switch onClick={this.onToggle} on={this.state.on} />
   }
 }
 
@@ -27,4 +34,4 @@ function Usage({
 }
 Usage.title = 'Build Toggle'
 
-export {Toggle, Usage as default}
+export { Toggle, Usage as default }
