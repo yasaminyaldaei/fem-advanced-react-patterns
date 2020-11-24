0027815b3e3e374641985f90df948e5d97c12685
Yasamin Yaldaei <yasaminyaldaei@gmail.com>
2020-11-24 17:18:52 +0330

    add exercise 5 - prop collection

--- src/__tests__/05.js
+++ src/__tests__/05.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import {renderToggle, Simulate} from '../../test/utils'
-import Usage from '../exercises-final/05'
-// import Usage from '../exercises/05'
+import { renderToggle, Simulate } from '../../test/utils'
+// import Usage from '../exercises-final/05'
+import Usage from '../exercises/05'
 
 test('renders a toggle component', () => {
   const handleToggle = jest.fn()
-  const {toggleButton, toggle} = renderToggle(
+  const { toggleButton, toggle } = renderToggle(
     <Usage onToggle={handleToggle} />,
   )
   expect(toggleButton).toBeOff()
@@ -17,7 +17,7 @@ test('renders a toggle component', () => {
 
 test('can also toggle with the custom button', () => {
   const handleToggle = jest.fn()
-  const {toggleButton, getByLabelText} = renderToggle(
+  const { toggleButton, getByLabelText } = renderToggle(
     <Usage onToggle={handleToggle} />,
   )
   expect(toggleButton).toBeOff()
@@ -36,8 +36,8 @@ test('can also toggle with the custom button', () => {
 /*
 http://ws.kcd.im/?ws=react%20patterns&e=05&em=yasaminyaldaei@gmail.com
 */
-test.skip('I submitted my elaboration and feedback', () => {
-  const submitted = false // change this when you've submitted!
+test('I submitted my elaboration and feedback', () => {
+  const submitted = true // change this when you've submitted!
   expect(submitted).toBe(true)
 })
 ////////////////////////////////
