c38b06363ae44b4f0b29dce25f17835afeca646e
Yasamin Yaldaei <yasaminyaldaei@gmail.com>
2020-12-10 15:44:51 +0330

    add exercise 12 - provider pattern

--- src/__tests__/11.js
+++ src/__tests__/11.js
@@ -53,8 +53,8 @@ test('can still use the render prop API', () => {
 /*
 http://ws.kcd.im/?ws=react%20patterns&e=11&em=yasaminyaldaei@gmail.com
 */
-test.skip('I submitted my elaboration and feedback', () => {
-  const submitted = false // change this when you've submitted!
+test('I submitted my elaboration and feedback', () => {
+  const submitted = true // change this when you've submitted!
   expect(submitted).toBe(true)
 })
 ////////////////////////////////
